929255d03c4f4cc8927afebc5ee091a0a6ed33fd
xutl <79109681@qq.com>
2016-09-14 10:11:11 +0800

    update

---
+++ Module.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+namespace yuncms\link;
+
+/**
+ * Class Module
+ * @package yuncms\link
+ */
+class Module extends \yii\base\Module
+{
+
+}
