02715799d4deee81b918c85dde07274af50a2c56
xutl <79109681@qq.com>
2016-09-21 14:42:52 +0800

    c

--- Module.php
+++ Module.php
@@ -6,6 +6,8 @@
  */
 namespace yuncms\link;
 
+use yuncms\link\models\Link;
+
 /**
  * Class Module
  * @package yuncms\link
@@ -13,4 +15,17 @@ namespace yuncms\link;
 class Module extends \yii\base\Module
 {
     public $controllerNamespace = 'yuncms\link\controllers';
+
+    /**
+     * 获取连接
+     * @param $type
+     * @param bool $isLogo
+     * @param int $limit
+     * @return array|models\Link[]
+     */
+    public static function getLinks($type, $isLogo = false, $limit = 10)
+    {
+        $query = Link::find()->where(['type' => $type])->andWhere(['not', 'logo', null])->orderBy(['frequency' => SORT_DESC])->limit($limit);
+        return $query->all();
+    }
 }
