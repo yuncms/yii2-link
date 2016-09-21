b6d960a80722a57788d365675767fdf0d80af11c
xutl <79109681@qq.com>
2016-09-21 14:43:16 +0800

    cc

--- migrations/m160921_063811_add_backend_menu.php
+++ migrations/m160921_063811_add_backend_menu.php
@@ -7,12 +7,12 @@ class m160921_063811_add_backend_menu extends Migration
     public function up()
     {
         $this->batchInsert('{{%admin_menu}}', ['id', 'name', 'parent', 'route', 'icon', 'sort', 'data'], [
-            [86, '连接管理', 8, '/link/index', 'fa-link', NULL, NULL],
+            [80, '连接管理', 8, '/link/index', 'fa-link', NULL, NULL],
         ]);
         $this->batchInsert('{{%admin_menu}}', ['name', 'parent', 'route', 'visible', 'sort'], [
-            ['连接查看', 45, '/link/view', 0, NULL],
-            ['创建连接', 45, '/link/create', 0, NULL],
-            ['更新连接', 45, '/link/update', 0, NULL],
+            ['连接查看', 80, '/link/view', 0, NULL],
+            ['创建连接', 80, '/link/create', 0, NULL],
+            ['更新连接', 80, '/link/update', 0, NULL],
             ['连接设置', 80, '/link/setting', 0, NULL],
         ]);
     }
