629100137814b93f073803b24d1e8f6eab36dbf4
xuchao <85825770@qq.com>
2017-02-04 16:33:11 +0800

    update

--- migrations/m160921_063811_add_backend_menu.php
+++ migrations/m160921_063811_add_backend_menu.php
@@ -7,27 +7,28 @@ class m160921_063811_add_backend_menu extends Migration
     public function up()
     {
         $this->insert('{{%admin_menu}}', [
-            'id' => 80,
             'name' => '连接管理',
             'parent' => 8,
             'route' => '/link/link/index', 'icon' => 'fa-link', 'sort' => null, 'data' => null]);
+        $id = (new \yii\db\Query())->select(['id'])->from('{{%admin_menu}}')->where(['name' => '连接管理', 'parent' => 8,])->scalar($this->getDb());
 
         $this->batchInsert('{{%admin_menu}}', ['name', 'parent', 'route', 'visible', 'sort'], [
-            ['连接查看', 80, '/link/link/view', 0, NULL],
-            ['创建连接', 80, '/link/link/create', 0, NULL],
-            ['更新连接', 80, '/link/link/update', 0, NULL],
-            ['连接设置', 80, '/link/link/setting', 0, NULL],
-            ['类别设置', 80, '/link/type/setting', 0, NULL],
-            ['创建类别', 80, '/link/type/create', 0, NULL],
-            ['更新类别', 80, '/link/type/update', 0, NULL],
-            ['查看类别', 80, '/link/type/view', 0, NULL],
+            ['连接查看', $id, '/link/link/view', 0, NULL],
+            ['创建连接', $id, '/link/link/create', 0, NULL],
+            ['更新连接', $id, '/link/link/update', 0, NULL],
+            ['连接设置', $id, '/link/link/setting', 0, NULL],
+            ['类别设置', $id, '/link/type/setting', 0, NULL],
+            ['创建类别', $id, '/link/type/create', 0, NULL],
+            ['更新类别', $id, '/link/type/update', 0, NULL],
+            ['查看类别', $id, '/link/type/view', 0, NULL],
         ]);
     }
 
     public function down()
     {
-        $this->delete('{{%admin_menu}}', ['id' => 80,]);
-        $this->delete('{{%admin_menu}}', ['parent' => 80,]);
+        $id = (new \yii\db\Query())->select(['id'])->from('{{%admin_menu}}')->where(['name' => '连接管理', 'parent' => 8,])->scalar($this->getDb());
+        $this->delete('{{%admin_menu}}', ['parent' => $id]);
+        $this->delete('{{%admin_menu}}', ['id' => $id]);
     }
 
     /*
