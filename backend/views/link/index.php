f17c789551f182308ef591a41a29c84968d39552
Peter <xutongle@gmail.com>
2017-09-22 10:44:07 +0800

    update

--- backend/views/link/index.php
+++ backend/views/link/index.php
@@ -61,7 +61,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     'url:url',
                     // 'logo',
                     [
-                        'attribute' => 'user.username',
+                        'attribute' => 'user.nickname',
                         'label' => Yii::t('link', 'User'),
                     ],
                     // 'created_at',
